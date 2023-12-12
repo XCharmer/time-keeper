5348ef9bb30b083a23adc40c6c4c53c8905affbc
XCharmer <777xwizard@gmail.com>
2023-12-12 23:37:46 +0500

    Add wakeLock

--- script.js
+++ script.js
@@ -387,6 +387,23 @@ calendar.addEventListener('click', (e) => {
   if (clickedDay) selectDay(clickedDay);
 });
 
+const canWakeLock = () => 'wakeLock' in navigator;
+let wakelock;
+async function lockWakeState() {
+  if (!canWakeLock()) return;
+  try {
+    wakelock = await navigator.wakeLock.request();
+    wakelock.addEventListener('release', () => {
+      console.log('Screen Wake State Locked:', !wakelock.released);
+    });
+    console.log('Screen Wake State Locked:', !wakelock.released);
+  } catch (e) {
+    console.error('Failed to lock wake state with reason:', e.message);
+  }
+}
+
+lockWakeState();
+
 const initButtons = function () {
   learnStartButton.addEventListener('click', (e) => {
     console.log(e);
